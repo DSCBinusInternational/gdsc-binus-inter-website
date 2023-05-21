8c047b01b42f6266a78b782318c6638064927123
Izam Ghali <izamghali18@gmail.com>
2023-05-21 15:03:58 +0700

    add infinite scroll. It's autoplay

---
+++ script.js
@@ -0,0 +1,12 @@
+gsap.to("#cards-first-row", {
+    x: "-100%",
+    ease: "linear",
+    duration: 40, 
+    repeat: -1,
+})
+gsap.to("#cards-second-row", {
+    x: "-100%",
+    ease: "linear",
+    duration: 40, 
+    repeat: -1,
+})
